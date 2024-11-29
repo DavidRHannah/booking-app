166b19965558758021d217f94edb370f7c1e9b6f
David Hannah <dhann013@ucr.edu>
2024-11-28 19:35:39 -0800

    add Header, Footer, and RoomPage

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
+
+export default function Home() {
+  return (
+    <>
+      <Heading title="Room Availability" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (<p>No rooms available at the moment</p>)}
+    </>
+  );
+}
